c098022bd9e551820d44a3552769d26e07a1a333
Soumwadeep Guha <soumwadeep@gmail.com>
2023-02-20 14:59:06 +0530

    Added Profile Files

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div className='container'>
-      <div className='profile-box'>
-        <div className='row'>
-            <div className='col-sm'>
-            <i className="fa fa-user-o" aria-hidden="true" id='iconfix'></i>
-            <br/>
-            
-            </div>
+    <div className="container">
+      <div className="profile-box">
+        <div className="row">
+          <div className="col-sm">
+            <i className="fa fa-user-o" aria-hidden="true" id="iconfix"></i>
+            <br />
+            <p className="mx-4" style={{ color: "rgba(71, 99, 253, 1)" }}>
+              Edit Picture
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
