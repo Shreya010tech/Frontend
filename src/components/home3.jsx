64010c063668ff8e235419e629c5fe451d1f4511
Alik Sarkar <88977627+Aliksarkar18@users.noreply.github.com>
2023-02-20 14:56:19 +0530

    created home3 page

---
+++ src/components/home3.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import homeimg from "../img/homeimg.png";
+const Home3 = () => {
+  return (
+   <div className="container" id="fixheight">
+        <div className="left">
+           <div>
+            <h1>Welcome to <br>
+                Siliguri Hotel Management Association</h1>
+            <h3>Let’s make ourself best <br>
+                & make ordinary, extraordinary</h3>
+           </div>
+        </div>
+        <div className="right">
+            <img src={homeimg} className="imgfix"/>
+        </div>
+    </div>
+  );
+};
+
+export default Home3;
