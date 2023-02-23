07a9e9c2daa68bbca9635b77a8a0e4093912334b
Prittha Dutta <pritthadatta@gmail.com>
2023-02-24 02:26:35 +0530

    Fixed minor bugs in Notice Page

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -5,27 +5,27 @@ const Notice = () => {
     return (
         <div>
             <div className='bg-light vh-100'>
-                <nav class="navbar sticky-top navbar navbar-expand-lg bg-light">
-                    <div class="container-fluid">
-                        <div class="navbar-brand d-flex align-items-center">
-                            <a class="text-primary" href="/#">
-                                <i class="bx bx-chevrons-left"></i>
+                <nav className="navbar sticky-top navbar navbar-expand-lg bg-light">
+                    <div className="container-fluid">
+                        <div className="navbar-brand d-flex align-items-center">
+                            <a className="text-primary" href="/#">
+                                <i className="bx bx-chevrons-left"></i>
                             </a>
-                            <h5 class="text-primary">Notice</h5>
+                            <h5 className="text-primary">Notice</h5>
                         </div>
                     </div>
                 </nav>
-                <div class="container bg-skyblue border-radius-10 height-550 p-2">
-                    <div class="d-flex align-items-center justify-content-center">
-                        <h3 class="text-primary align-text-center">Notices</h3>
+                <div className="container bg-skyblue border-radius-10 height-550 p-2">
+                    <div className="d-flex align-items-center justify-content-center">
+                        <h3 className="text-primary align-text-center">Notices</h3>
                     </div>
-                    <div class="flex-column px-5">
-                        <div class="height-50 background-gray border-radius-10 mt-4"></div>
-                        <div class="height-50 background-gray border-radius-10 mt-4"></div>
-                        <div class="height-50 background-gray border-radius-10 mt-4"></div>
-                        <div class="height-50 background-gray border-radius-10 mt-4"></div>
-                        <div class="height-50 background-gray border-radius-10 mt-4"></div>
-                        <div class="height-50 background-gray border-radius-10 mt-4"></div>
+                    <div className="flex-column px-5">
+                        <div className="height-50 background-gray border-radius-10 mt-4"></div>
+                        <div className="height-50 background-gray border-radius-10 mt-4"></div>
+                        <div className="height-50 background-gray border-radius-10 mt-4"></div>
+                        <div className="height-50 background-gray border-radius-10 mt-4"></div>
+                        <div className="height-50 background-gray border-radius-10 mt-4"></div>
+                        <div className="height-50 background-gray border-radius-10 mt-4"></div>
                     </div>
                 </div>
             </div>
