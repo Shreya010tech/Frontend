82a6e0c26917933f9e3dfd5bb52be1ae2c61fb01
Soumwadeep Guha <soumwadeep@gmail.com>
2023-02-24 20:37:09 +0530

    Added build

---
+++ src/components/Laundry.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Laundry = () => {
+  return (
+    <div className="formA">
+        <h2>House Keeping <br/>Laundry</h2>
+        
+    </div>
+  )
+}
+
+export default Laundry
