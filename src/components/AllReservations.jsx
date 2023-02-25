ffc5bcda288705dffaaaf079219f62e63d855431
Prasun Roy <prasunroy212@gmail.com>
2023-02-25 18:56:02 +0530

    Added get reserv.data endpoints in AllRes. Page

--- src/components/AllReservations.jsx
+++ src/components/AllReservations.jsx
@@ -1,8 +1,58 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useEffect } from "react";
 import "../CustomCss/Reservation.css";
+import Localbase from "localbase";
+let db = new Localbase("hmctdb");
+db.config.debug = false;
 
 const AllReservations = () => {
+
+
+  useEffect(() => {
+    async function fetchInitialData() {
+      let reservationResData = await getAllReservationData();
+      // let reservationResData = await getReservationData('Ok','o','99999');
+      console.log(reservationResData);
+    }
+    fetchInitialData();
+  }, [])
+  
+
+  // GetAll : Get all reservation data (orderBy: recent booking first)
+  // params : none 
+  // return : 1. {success: true, data: [<Array of all reservation data>{},{}]}        IF ALL OK
+  //          2. {success: false, msg: 'Something Went Wrong'}                        IF INTERNAL SERVER ERROR
+  const getAllReservationData = async()=>{
+    try{
+      let reservationData = await db.collection('reservation').orderBy('bookingdate', 'desc').get();
+      return {success: true, data: reservationData}
+    }catch(e){
+      console.log("AllReservationPageError (getAllReservationData) : ",e);
+      return {success:false, msg: "Something Went Wrong"}
+    }
+  }
+
+
+  // Get : Get all reservation data based on a particular guest (orderBy: recent booking first)
+  // params : guestfirstname, guestlastname, guestphoneno
+  // return : 1. {success: true, data: [<Array of all reservation data>{},{}]}        IF ALL OK
+  //          2. {success: false, msg: 'Something Went Wrong'}                        IF INTERNAL SERVER ERROR
+  const getReservationData = async(guestfirstname, guestlastname, guestphoneno) =>{
+    try{
+      let reservationData = await db.collection('reservation').orderBy('bookingdate', 'desc').get();
+
+      const filteredData = reservationData.filter(reservation => {
+        return reservation.name.firstname == guestfirstname && reservation.name.lastname == guestlastname && reservation.phoneno == guestphoneno;
+      });
+
+      return {success: true, data: filteredData}
+    }catch(e){
+      console.log("AllReservationPageError (getReservationData) : ",e);
+      return {success:false, msg: "Something Went Wrong"}
+    }
+  }
+
+
   return (
     <div>
       <div className="bg-light vh-100">
