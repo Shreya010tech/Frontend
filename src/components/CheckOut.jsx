7fac0f3c0625094c2782c1a79eefef99b7da99ea
Soumwadeep Guha <soumwadeep@gmail.com>
2023-02-26 05:40:17 +0530

    Fixed Classnames of checkout and remaining css

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -14,79 +14,79 @@ const CheckOut = () => {
           </div>
         </div>
       </nav>
-      <div class="containerA px-0" style={{ maxWidth: "100%" }}>
-        <div class="row1A">
-          <div class="lefta">
+      <div className="containerA px-0" style={{ maxWidth: "100%" }} id="checkoutbody">
+        <div className="row1A">
+          <div className="lefta">
             <form>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Guest Name:</label>
                 <input type="text" placeholder="Guest Name" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Travel Agent:</label>
                 <input type="text" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Company Name:</label>
                 <input type="text" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Mobile No:</label>
                 <input type="Text" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Billing:</label>
                 <input type="text" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>GST ID:</label>
                 <input type="text" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Bill No:</label>
                 <input type="text" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Confirmation No:</label>
                 <input type="text" placeholder="" required />
               </div>
             </form>
           </div>
-          <div class="righta">
+          <div className="righta">
             <form>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Orginal Bill Date:</label>
                 <input type="Date" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Room No:</label>
                 <input type="number" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>No of Rooms:</label>
                 <input type="number" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Room Rate:</label>
                 <input type="number" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>No of Guest:</label>
                 <input type="number" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Arrival Date:</label>
                 <input type="Date" placeholder="" required />
                 at
                 <input type="time" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Departure Date:</label>
                 <input type="date" placeholder="" required />
                 at
                 <input type="time" placeholder="" required />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Registration No:</label>
                 <input type="Number" placeholder="" required />
               </div>
@@ -94,8 +94,8 @@ const CheckOut = () => {
           </div>
         </div>
         <br />
-        <div class="row2A">
-          <table>
+        <div className="row2A">
+          <table className="table">
             <tr bgcolor="#EDF8FD" align="center">
               <th width="100">Date</th>
               <th width="100">Description</th>
@@ -140,31 +140,31 @@ const CheckOut = () => {
             </tr>
           </table>
         </div>
-        <div class="row3A">
-          <div class="lefts">
+        <div className="row3A">
+          <div className="lefts">
             <form>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Casher:</label>
               </div>
             </form>
           </div>
-          <div class="rights">
+          <div className="rights">
             <form>
-              <div class="form-group">
+              <div className="form-group">
                 <label>Guest Signature:</label>
               </div>
             </form>
           </div>
         </div>
-        <div class="row4A">
-          <div class="form-check">
+        <div className="row4A">
+          <div className="form-check">
             <input
-              class="form-check-input"
+              className="form-check-input"
               type="checkbox"
               value=""
               id="flexCheckDefault"
             />
-            <label class="form-check-label" for="flexCheckDefault">
+            <label className="form-check-label" for="flexCheckDefault">
               I agree that I am liable for the following statement.
             </label>
           </div>
