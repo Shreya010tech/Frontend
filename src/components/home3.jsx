2c2b97f1c9fd192ba9a573889c2a909386bf14f9
Soumwadeep Guha <soumwadeep@gmail.com>
2023-02-27 06:54:06 +0530

    Added Image

--- src/components/home3.jsx
+++ src/components/home3.jsx
@@ -126,65 +126,15 @@ const Home3 = () => {
           <h3>
             Let’s make ourself best <br />& make ordinary, extraordinary
           </h3>
-          {/* <NavLink to="/Reservation">
-            <button className="btn btn-primary">Reservation</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/AllReservations">
-            <button className="btn btn-primary">All Reservations</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/ReservationConfirmation">
-            <button className="btn btn-primary">
-              Reservation Confirmation
-            </button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/Profile">
-            <button className="btn btn-primary">Profile</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/RoomAvailability">
-            <button className="btn btn-primary">Room Availability</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/Application">
-            <button className="btn btn-primary">Application</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/Notice">
-            <button className="btn btn-primary">Notice</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/GuestHistory">
-            <button className="btn btn-primary">Guest History</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/HistoryReports">
-            <button className="btn btn-primary">History Reports</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/CheckIn">
-            <button className="btn btn-primary">Check-In</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/Laundry">
-            <button className="btn btn-primary">Laundry</button>
-          </NavLink>
-          &nbsp;
-          <NavLink to="/Checkout">
-            <button className="btn btn-primary">Check-Out</button>
-          </NavLink>
-          &nbsp; */}
         </div>
       </div>
       <div className="right">
-        <i
+        <img
           id="iconcolor"
           mg
           src={homeimg}
           style={{
-            marginTop: "30%",
+            marginTop: "45%",
             width: "100%",
             minHeight: "500px",
             minWidth: "800px",
