1b1accbe310cccc7088e7231f40b07f7913eb1a3
Soumwadeep Guha <soumwadeep@gmail.com>
2023-02-27 06:51:07 +0530

    Added Full Navbar And Redesigned

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Profile from "./components/Profile";
 import Reservation from "./components/Reservation";
 import ReservationConfirmation from "./components/ReservationConfirmation";
 import RoomAvailability from "./components/RoomAvailability";
-import CheckIn from "./components/checkin";
+import CheckIn from "./components/CheckIn";
 import GuestHistory from "./components/GuestHistory";
 import HistoryReports from "./components/HistoryReports";
 import CheckOut from "./components/CheckOut";
@@ -36,7 +36,7 @@ const App = () => {
         <Route exact path="/Application" element={<Application />} />
         <Route exact path="/Notice" element={<Notice />} />
         <Route exact path="/GuestHistory" element={<GuestHistory />} />
-        <Route exact path="/HistoryReports" element={<HistoryReports />} />
+        <Route exact path="/Reports" element={<HistoryReports />} />
         <Route exact path="/Laundry" element={<Laundry />} />
         <Route exact path="/CheckIn" element={<CheckIn />} />
         <Route exact path="/Checkout" element={<CheckOut />} />
--- src/components/home3.jsx
+++ src/components/home3.jsx
@@ -7,14 +7,14 @@ const Home3 = () => {
     <div className="containerhome3" id="fixheight">
       <div className="left">
         <div>
-        <button
+          <button
             className="btn btn-primary"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasWithBothOptions"
             aria-controls="offcanvasWithBothOptions"
           >
-           <i className="fa fa-align-right" aria-hidden="true"></i>
+            <i className="fa fa-align-right" aria-hidden="true"></i>
           </button>
 
           <div
@@ -28,8 +28,15 @@ const Home3 = () => {
               <h5
                 className="offcanvas-title"
                 id="offcanvasWithBothOptionsLabel"
+                style={{ color: "black" }}
               >
-                <i className="fa fa-angle-double-right" aria-hidden="true"></i> Getting Started
+                <i
+                  id="iconcolor"
+                  className="fa fa-angle-double-right"
+                  aria-hidden="true"
+                  style={{ color: "#4763FD" }}
+                ></i>{" "}
+                Getting Started
               </h5>
               <button
                 type="button"
@@ -39,11 +46,77 @@ const Home3 = () => {
               ></button>
             </div>
             <div className="offcanvas-body">
-              <NavLink to="/Reservation" className="NavLink">
-              <i class="fa fa-id-card-o" aria-hidden="true"></i>
-              &nbsp;
-              Reservation
+              <NavLink to="/Reservation" className="NavLinks">
+                <i
+                  id="iconcolor"
+                  class="fa fa-id-card-o"
+                  aria-hidden="true"
+                ></i>
+                &nbsp; Reservation
               </NavLink>
+              <br />
+              <NavLink to="/CheckIn" className="NavLinks">
+                <i id="iconcolor" class="fa fa-sign-in" aria-hidden="true"></i>
+                &nbsp; Check In
+              </NavLink>
+              <br />
+              <NavLink to="/CheckOut" className="NavLinks">
+                <i id="iconcolor" class="fa fa-sign-in" aria-hidden="true"></i>
+                &nbsp; Check Out
+              </NavLink>
+              <br />
+              <NavLink to="/RoomAvailability" className="NavLinks">
+                <i id="iconcolor" class="fa fa-bed" aria-hidden="true"></i>
+                &nbsp; Room Availability
+              </NavLink>
+              <br />
+              <NavLink to="/HouseKeeping" className="NavLinks">
+                <i id="iconcolor" class="fa fa-user" aria-hidden="true"></i>
+                &nbsp; House Keeping
+              </NavLink>
+              <br />
+              <NavLink to="/FBService" className="NavLinks">
+                <i id="iconcolor" class="fa fa-cutlery" aria-hidden="true"></i>
+                &nbsp; F & B Service
+              </NavLink>
+              <br />
+              <NavLink to="/GuestHistory" className="NavLinks">
+                <i id="iconcolor" class="fa fa-book" aria-hidden="true"></i>
+                &nbsp; Guest History
+              </NavLink>
+              <br />
+              <NavLink to="/Reports" className="NavLinks">
+                <i
+                  id="iconcolor"
+                  class="fa fa-file-text"
+                  aria-hidden="true"
+                ></i>
+                &nbsp; Reports
+              </NavLink>
+              <br />
+              <NavLink to="/Application" className="NavLinks">
+                <i id="iconcolor" class="fa fa-pencil" aria-hidden="true"></i>
+                &nbsp; Application
+              </NavLink>
+              <br />
+              <NavLink to="/Notice" className="NavLinks">
+                <i
+                  id="iconcolor"
+                  class="fa fa-calendar-check-o"
+                  aria-hidden="true"
+                ></i>
+                &nbsp; Notice
+              </NavLink>
+              <br />
+              <NavLink to="/Profile" className="NavLinks">
+                <i
+                  id="iconcolor"
+                  class="fa fa-user-circle"
+                  aria-hidden="true"
+                ></i>
+                &nbsp; Profile
+              </NavLink>
+              <br />
             </div>
           </div>
           <h1>
@@ -106,7 +179,9 @@ const Home3 = () => {
         </div>
       </div>
       <div className="right">
-        <img
+        <i
+          id="iconcolor"
+          mg
           src={homeimg}
           style={{
             marginTop: "30%",
